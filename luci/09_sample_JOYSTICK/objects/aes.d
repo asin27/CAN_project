93ae3d15029ce78b831b58f11d40fc0b3599944c
jspkay <jspkay@gmail.com>
2021-11-06 18:21:21 +0100

    Generalization upgrade

--- luci/09_sample_JOYSTICK/objects/aes.d
+++ luci/09_sample_JOYSTICK/objects/aes.d
@@ -1,5 +1,5 @@
-.\objects\aes.o: security\aes.c
+.\objects\aes.o: ..\..\shared_libraries\security\aes.c
 .\objects\aes.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\objects\aes.o: security\aes.h
+.\objects\aes.o: ..\..\shared_libraries\security\aes.h
 .\objects\aes.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\aes.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
---
+++ luci/09_sample_JOYSTICK/objects/highcan.d
@@ -0,0 +1,15 @@
+.\objects\highcan.o: ..\..\shared_libraries\CAN\highcan.c
+.\objects\highcan.o: ..\..\shared_libraries\CAN\Drivers\include\lpc17xx_can.h
+.\objects\highcan.o: C:\Users\clem-\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.6.0\Device\Include\LPC17xx.h
+.\objects\highcan.o: C:\Users\clem-\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm3.h
+.\objects\highcan.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\highcan.o: C:\Users\clem-\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\highcan.o: C:\Users\clem-\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\highcan.o: C:\Users\clem-\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\highcan.o: C:\Users\clem-\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\highcan.o: C:\Users\clem-\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.6.0\Device\Include\system_LPC17xx.h
+.\objects\highcan.o: ..\..\shared_libraries\CAN\Drivers\include\lpc_types.h
+.\objects\highcan.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\highcan.o: .\Core\../GLCD/GLCD.h
+.\objects\highcan.o: ..\..\shared_libraries\CAN\headers\./highcan.h
+.\objects\highcan.o: ..\..\shared_libraries\CAN\headers\././can.h
