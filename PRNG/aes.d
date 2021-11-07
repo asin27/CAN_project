3d65c4c671d9f229f7fab138878afc7411152c84
a_sin <sindoniandrea8@gmail.com>
2021-11-07 17:41:46 +0100

    cleaning up

--- PRNG/aes.d
+++
@@ -1,5 +0,0 @@
-.\aes.o: ..\shared_libraries\security\aes.c
-.\aes.o: C:\Keil\ARM\ARMCC\Bin\..\include\string.h
-.\aes.o: ..\shared_libraries\security\aes.h
-.\aes.o: C:\Keil\ARM\ARMCC\Bin\..\include\stdint.h
-.\aes.o: C:\Keil\ARM\ARMCC\Bin\..\include\stddef.h
--- PRNG/security.d
+++
@@ -1,8 +0,0 @@
-.\security.o: ..\shared_libraries\security\security.c
-.\security.o: ..\shared_libraries\security\security.h
-.\security.o: C:\Keil\ARM\ARMCC\Bin\..\include\stddef.h
-.\security.o: ..\shared_libraries\security\sha256.h
-.\security.o: ..\shared_libraries\security\aes.h
-.\security.o: C:\Keil\ARM\ARMCC\Bin\..\include\stdint.h
-.\security.o: C:\Keil\ARM\ARMCC\Bin\..\include\stdio.h
-.\security.o: C:\Keil\ARM\ARMCC\Bin\..\include\string.h
